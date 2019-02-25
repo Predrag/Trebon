0c03403c6b1ae39e54054f47ea6530894918d64b
Predrag <460694+Predrag@users.noreply.github.com>
2019-02-25 22:45:01 +0100

    doplnenie K_kriticke

--- vypocty.py
+++ vypocty.py
@@ -1,21 +1,12 @@
 from nacitanie_dat import Nacitanie_dat
 import pandas as pd
-import operator
-import numpy as np
+import matplotlib.pyplot as plt
 
 
 class Statistika(Nacitanie_dat):
 
     def __init__(self, vlnova_dlzka):
         super().__init__(vlnova_dlzka)
-
-    # def vypocet_priemeru(self):
-    #     slovnik = dict()
-    #     vrat_slovnik = dict()
-    #     for key, value in self.vlozenie_parcialnych_data_frejmov_do_slovnikov().items():
-    #         vrat_slovnik[key] = [pd.DataFrame.mean(value[s]) for s in range(0, len(value))]
-    #         slovnik.update(vrat_slovnik)
-    #     return slovnik
 
     def vypocet_priemeru(self):
         priemer_zoznam = list()
@@ -28,20 +19,11 @@ class Statistika(Nacitanie_dat):
                 ds = df['Abs'].mean()
                 priemer_zoznam.append(ds)
 
-            # n = len(self.rozdelenie_data_frejmov_na_mensie_celky()[0])
-            # rozdeleny_priemer_zoznam = [priemer_zoznam[i * n:(i + 1) * n] for i in range((len(priemer_zoznam) + n - 1) // n)]
+            n = len(self.rozdelenie_data_frejmov_na_mensie_celky()[0])
+            rozdeleny_priemer_zoznam = [priemer_zoznam[i * n:(i + 1) * n] for i in
+                                        range((len(priemer_zoznam) + n - 1) // n)]
 
-        # return rozdeleny_priemer_zoznam
-        return priemer_zoznam
-
-
-    # def vypocet_strednej_odchylky(self):
-    #     slovnik = dict()
-    #     vrat_slovnik = dict()
-    #     for key, value in self.vlozenie_parcialnych_data_frejmov_do_slovnikov().items():
-    #         vrat_slovnik[key] = [pd.DataFrame.std(value[s]) for s in range(0, len(value))]
-    #         slovnik.update(vrat_slovnik)
-    #     return slovnik
+        return rozdeleny_priemer_zoznam
 
     def vypocet_strednej_odchylky(self):
         stredna_odchylka_zoznam = list()
@@ -54,70 +36,40 @@ class Statistika(Nacitanie_dat):
                 ds = df['Abs'].std()
                 stredna_odchylka_zoznam.append(ds)
 
-            # n = len(self.rozdelenie_data_frejmov_na_mensie_celky()[0])
-            # rozdelena_stredna_odchylka_zoznam = [stredna_odchylka_zoznam[i * n:(i + 1) * n] for i in range((len(stredna_odchylka_zoznam) + n - 1) // n)]
+            n = len(self.rozdelenie_data_frejmov_na_mensie_celky()[0])
+            rozdelena_stredna_odchylka_zoznam = [stredna_odchylka_zoznam[i * n:(i + 1) * n] for i in
+                                                 range((len(stredna_odchylka_zoznam) + n - 1) // n)]
 
-        # return rozdelena_stredna_odchylka_zoznam
-        return stredna_odchylka_zoznam
-
-    # def vypocet_K_crit(self):
-    #     vrat_slovnik = dict()
-    #     vlozenie = self.vlozenie_parcialnych_data_frejmov_do_slovnikov()
-    #     for key, value in vlozenie.items():
-    #             # vlozenie[key] = value[0]
-    #             vlozenie[key] = [abs(value[j] - self.vypocet_priemeru()[0][j]) / self.vypocet_strednej_odchylky()[0][j] for j in range(len(value))]
-    #             vrat_slovnik.update(vlozenie)
-    #     return vrat_slovnik
-
-    # def vypocet_K_crit(self):
-    #     vrat_slovnik = dict()
-    #     vrat_slovnik2 = dict()
-    #     f = list()
-    #     vlozenie = self.vlozenie_parcialnych_data_frejmov_do_slovnikov()
-    #     j = 0
-    #     # v = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    #     for key, value in vlozenie.items():
-    #         vlozenie[key] = [
-    #             abs((value[j] - self.vypocet_priemeru()[key][j]) / self.vypocet_strednej_odchylky()[key][j]) for j in
-    #             range(0, len(value))]
-    #         # vrat_slovnik.update(vlozenie)
-    #         f.append(vlozenie[key])
-    #
-    #     pocitadlo = 0
-    #     g = list()
-    #     z = list()
-    #     for key, value in vlozenie.items():
-    #         for i in f:
-    #             for j in i:
-    #                 s = j.where(lambda x: x > 1.996,
-    #                             lambda x: self.vlozenie_parcialnych_data_frejmov_do_slovnikov()[key][pocitadlo][::])
-    #                 pocitadlo += 1
-    #                 g = s.mask(s > 1.996)
-    #                 z.append(g)
-    #             pocitadlo = 0
-    #     return z
-    #
-    #     for key, value in vlozenie.items():
-    #         vrat_slovnik[key] = z
+        return rozdelena_stredna_odchylka_zoznam
 
     def k_kriticke(self):
-        velkost_zoznamu = len(self.rozdelenie_data_frejmov_na_mensie_celky())
-        # print(self.rozdelenie_data_frejmov_na_mensie_celky()[0])
-        vysledok_result = list()
-        for i in range(velkost_zoznamu):
-            result = map(lambda x, y: abs(x - y), self.rozdelenie_data_frejmov_na_mensie_celky()[i], self.vypocet_priemeru())
-            # result2 = map(lambda x, y: x/y, result, self.vypocet_strednej_odchylky())
-            for j in result:
-                print(j)
+
+        vysledne_k_po_podmienke = list()
+        d = []
+        p = self.vypocet_priemeru()
+        s = self.vypocet_strednej_odchylky()
+
+        for i in self.rozdelenie_data_frejmov_na_mensie_celky():
+            d.append(i)
+
+        for j in range(0, len(self.rozdelenie_data_frejmov_na_mensie_celky())):
+            for k in range(0, len(self.rozdelenie_data_frejmov_na_mensie_celky()[0])):
+                docasne_k = abs(d[j][k] - p[j][k]) / s[j][k]
+                df = pd.DataFrame(docasne_k)
+                ds = df.where((df['Abs'] > 1.996), d[j][k], axis=0)
+                da = pd.DataFrame(ds)
+                dc = da.where(ds['Abs'] < 1.996)
+
+                dc['Cas'] = 0
+
+                dc.plot(x='Cas', y='Abs')
+                # plt.show()
+                vysledne_k_po_podmienke.append(dc)
+
+        return vysledne_k_po_podmienke
 
 
-
-
-
-
-
-
-
+cas = [1, 2]
 
 b = Statistika(750)
 # print(b.vypocet_priemeru())
